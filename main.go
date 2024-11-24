7e32bd4f7838bf70e51a63f09fe98b42a7ba0716
flaviusp23 <flavius.paltin@yahoo.com>
2024-11-24 16:35:46 +0200

    new deck

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	cards := deck{newCard(), "Ace of Diamonds"}
-	cards = append(cards, newCard())
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
